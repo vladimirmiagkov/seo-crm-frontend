69438f8b6688a4a3d54ac3ec9126d1041e8c8fc1
vlad82 <impulsvideo@yandex.ru>
2017-02-18 21:31:31 +0300

    load Sites

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,82 @@
 import {Component, OnInit} from '@angular/core';
 import {MenuItem} from 'primeng/primeng';
+import {Header} from 'primeng/primeng';
+import {Footer} from 'primeng/primeng';
+import {LazyLoadEvent} from 'primeng/components/common/api';
+
+import {SiteService} from '../site/site/sites.service';
+import {Site} from '../site/site/site.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [SiteService]
 })
 export class HomeComponent implements OnInit {
-  private viewSettingsMenu: MenuItem[];
-  //private viewPager: number = 5;
-  private viewDateFrom: Date;
-  private viewDateTo: Date;
+  public viewSettingsMenu: MenuItem[];
+  public viewPager: any;
+  public viewDateFrom: Date;
+  public viewDateTo: Date;
+  public readonly SITEVIEW_PAGERIDENTIFIER: string = 'siteview-pager-';
 
-  constructor() {
+  public objs: Site[];
+  public currentObj: Site = new Site();
+  public isNewObj: boolean;
+
+  constructor(private siteService: SiteService) {
   }
 
   ngOnInit() {
+    this.initViewSettingsMenu();
+    this.initViewPager();
+    this.loadSites();
+  }
+
+  public loadSites() {
+    //console.dir(this.viewPager);
+    // this.siteService.get(
+    //   this.viewPager.page * this.viewPager.rows,
+    //   this.viewPager.rows,
+    //   this.viewDateFrom,
+    //   this.viewDateTo
+    // ).subscribe(
+    //   result => {
+    //     console.dir(result);
+    //     //this.objs = result;
+    //   },
+    //   err => {
+    //     console.log(err);
+    //   });
+  }
+
+  public loadCarsLazy(event: LazyLoadEvent) {
+    //event.first = First row offset
+    //event.rows = Number of rows per page
+    //event.sortField = Field name to sort with
+    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+    //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+
+    // if(this.datasource) {
+    //   this.cars = this.datasource.slice(event.first, (event.first + event.rows));
+    // }
+  }
+
+  // Fire up on pager change
+  public onPaginate(event) {
+    console.dir(event);
+    this.viewPager.first = event.first;
+    this.viewPager.rows = event.rows;
+    this.viewPager.page = event.page;
+    this.viewPager.pageCount = event.pageCount;
+
+    localStorage.setItem(this.SITEVIEW_PAGERIDENTIFIER + 'rows', this.viewPager.rows);
+    localStorage.setItem(this.SITEVIEW_PAGERIDENTIFIER + 'page', this.viewPager.page);
+
+    this.loadSites();
+  }
+
+  private initViewSettingsMenu() {
     this.viewSettingsMenu = [
       {label: 'Display/sort sites', icon: 'fa-filter',},
       {label: 'Highlight comments', icon: 'fa-commenting-o',},
@@ -34,11 +95,25 @@ export class HomeComponent implements OnInit {
     ];
   }
 
-  public viewPagerPaginate(event) {
-    console.dir(event);
-    //event.first = Index of the first record
-    //event.rows = Number of rows to display in new page
-    //event.page = Index of the new page
-    //event.pageCount = Total number of pages
+  private initViewPager() {
+    this.viewPager = {
+      first: 0, // Index of the first record
+      rows: 10, // Number of rows to display in new page
+      page: 0, // Index of the new page
+      pageCount: 0, // Total number of pages
+      totalRecords: 100, // Total records
+      rowsPerPageOptions: [1, 5, 10, 20, 50, 100], // Objects per page
+      pageLinkSize: 5, // Max visible pages in navigation
+    };
+
+    let rows = localStorage.getItem(this.SITEVIEW_PAGERIDENTIFIER + 'rows');
+    if (null !== rows) {
+      this.viewPager.rows = +rows;
+    }
+    let page = localStorage.getItem(this.SITEVIEW_PAGERIDENTIFIER + 'page');
+    if (null !== page) {
+      this.viewPager.page = +page;
+      this.viewPager.first = +page * +rows;
+    }
   }
 }
--- src/app/site/site/sites.service.ts
+++ src/app/site/site/sites.service.ts
@@ -9,8 +9,22 @@ export class SiteService {
   public constructor(private apiService: ApiService) {
   }
 
-  public get(): Observable<Site[]> {
-    return this.apiService.get('/site');
+  /**
+   * @param start Pager: start from item
+   * @param limit Pager: count items to fetch
+   * @param dateFrom Start date for sites data
+   * @param dateTo Stop date for sites data
+   * @returns {Observable<any>}
+   */
+  public get(start: any = '', limit: any = '', dateFrom: any = '', dateTo: any = '',): Observable<Site[]> {
+    let path: string = '/site?'
+      + '&start=' + start
+      + '&limit=' + limit
+      + '&datefrom=' + dateFrom
+      + '&dateto=' + dateTo;
+    console.log(path);
+
+    return this.apiService.get(path);
   }
 
   public update(site: Site): Observable<Site> {
